9ff1721d604541e97e61c210ea0a54231ada83b6
unknown <akskejdtod@gmail.com>
2022-03-23 18:18:32 +0900

    2022/03/23

--- Farmstory2/src/main/java/kr/co/farmstory2/service/board/ViewService.java
+++ Farmstory2/src/main/java/kr/co/farmstory2/service/board/ViewService.java
@@ -1,15 +1,41 @@
-package kr.co.farmstory2.service.board;
+package kr.co.farmstory2.service;
+
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import kr.co.farmstory2.config.DBConfig;
 import kr.co.farmstory2.controller.CommonService;
+import kr.co.farmstory2.dao.ArticleDao;
+import kr.co.farmstory2.vo.ArticleVo;
+import kr.co.farmstory2.vo.UserVo;
 
 public class ViewService implements CommonService {
 
 	@Override
 	public String businessProc(HttpServletRequest req, HttpServletResponse resp) {
-		return "/board/view.jsp";
+		
+		// 로그인 체크
+		HttpSession sess = req.getSession();
+		UserVo userVo = (UserVo) sess.getAttribute("sessUser");
+		
+		if(userVo == null) {
+			return "redirect:/Board2/user/login.do";
+		}else {
+			String no = req.getParameter("no");
+			
+			ArticleDao dao = ArticleDao.getInstance();
+			
+			ArticleVo article = dao.selectArticle(no);
+			List<ArticleVo> comments = dao.selectComments(no);
+			
+			req.setAttribute("article", article);
+			req.setAttribute("comments", comments);
+			
+			return "/view.jsp";
+		}
 	}
 
 }
